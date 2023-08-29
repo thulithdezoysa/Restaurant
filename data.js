a5c5cad0df16a93c7320aeb2911212a1ffc29595
Thulith de Zoysa <139491011+thulithdezoysa@users.noreply.github.com>
2023-08-29 14:28:47 +0100

    bug fix for netlify

--- data.js
+++ data.js
@@ -4,41 +4,41 @@ export const menuArray = [
         ingredients: ["Milk", "Water", "Roasted Arabica Beans"],
         id: 0,
         price: 3.99,
-        emoji: "./public/assets/latte-coffee.jpg"
+        emoji: "./assets/latte-coffee.jpg"
     },
     {
         name: "Cappuccino",
         ingredients: ["Milk", "Water", "Roasted Arabica", "Chocolate Powder"],
         price: 4.49,
-        emoji: "./public/assets/cappuccino.jpg",
+        emoji: "./assets/cappuccino.jpg",
         id: 1
     },
         {
         name: "Espresso",
         ingredients: [ "Water", "Roasted Arabica Coffee beans"],
         price: 3.10,
-        emoji: "./public/assets/espresso.jpg",
+        emoji: "./assets/espresso.jpg",
         id: 2
     },
     {
         name: "Mocha",
         ingredients: ["Milk", "Water", "Chocolate Powder", "Roasted Arabica"],
         price: 3.49,
-        emoji: "./public/assets/mocha.jpg",
+        emoji: "./assets/mocha.jpg",
         id: 3
     },
     {
         name: "Black Coffee",
         ingredients: ["Water", "Ground coffee"],
         price: 2.49,
-        emoji: "./public/assets/black-coffee.jpg",
+        emoji: "./assets/black-coffee.jpg",
         id: 4
     },
     {
         name: "Macchiato",
         ingredients: ["Milk", "Water",  "Roasted Arabica"],
         price: 3.49,
-        emoji: "./public/assets/macchiato.jpg",
+        emoji: "./assets/macchiato.jpg",
         id: 5
     },
     
